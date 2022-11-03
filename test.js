1bccb69d7be9bc3ceafaadadd727630aecb2a128
Andris Reinman <andris@reinman.eu>
2022-11-03 11:13:06 +0200

    update

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ if (process.argv[2]) {
     sourceEml = Buffer.from('Subject: Hello\r\n  World\r\n  Again\ndef\rghi\r\r\r\n\neeeee\n\n');
 }
 
-let ysourceEml = Buffer.from(
+sourceEml = Buffer.from(
     'Content-type: multipart/mixed; boundary=ABC\r\n' +
         '\r\n' +
         '--ABC\r\n' +
@@ -28,18 +28,18 @@ let ysourceEml = Buffer.from(
         '--ABC'
 );
 
-ysourceEml = Buffer.from(
+sourceEml = Buffer.from(
     (
         'Content-type: multipart/mixed; boundary=ABC\r\n' +
         '\r\n' +
         '--ABC\r\n' +
         'Content-type: text/plain; charset=utf-8\r\n' +
         '\r\n' +
-        'ÕÄÖÜ\r\n' +
+        'ÕÄÖÜ1\r\n' +
         '--ABC\r\n' +
         'Content-type: text/plain; charset=utf-8\r\n' +
         '\r\n' +
-        'ÕÄÖÜ\r\n\r\n' +
+        'ÕÄÖÜ2\r\n\r\n' +
         '--ABC--\r\n'
     ).replace(/\r?\n/g, '\n')
 );
@@ -53,15 +53,21 @@ async function main() {
     let walk = (node, level) => {
         let prefix = ' '.repeat(level * 2);
         console.log(`${prefix}${node.contentType}`);
-        let childNodes = node.getChildNodes();
-        if (childNodes) {
-            for (let childNode of childNodes) {
+
+        if (node.multipartType) {
+            for (let childNode of node.childNodes) {
                 walk(childNode, level + 1);
             }
+        } else {
+            console.log('<<<<');
+            process.stdout.write(node.content);
+            console.log('\n>>>>');
         }
     };
 
     walk(mp, 0);
+
+    console.log('WALKED');
 
     let output = await mp.serialize();
 
@@ -71,7 +77,9 @@ async function main() {
 
     let n1 = MimeNode.create(
         'multipart/mixed',
-        {},
+        {
+            subject: 'Mõnel pool elavad ka kalan kõrbenult 🤔'
+        },
         {
             lineBr: '\n'
         }
@@ -86,12 +94,15 @@ async function main() {
     );
 
     let n3 = MimeNode.create('text/plain', { encoding: 'base64' });
-    n3.setContent(Buffer.alloc(10 * 1024));
+    n3.content = Buffer.alloc(10 * 1024);
 
-    let n4 = MimeNode.create('text/html', { encoding: 'quoted-printable' });
-    n4.setContent(
-        'Οὐχὶ ταὐτὰ παρίσταταί μοι γιγνώσκειν, ὦ ἄνδρες ᾿Αθηναῖοι, ὅταν τ᾿ εἰς τὰ πράγματα ἀποβλέψω καὶ ὅταν πρὸς τοὺς λόγους οὓς ἀκούω· τοὺς μὲν γὰρ λόγους περὶ τοῦ τιμωρήσασθαι Φίλιππον ὁρῶ γιγνομένους, τὰ δὲ πράγματ᾿'
-    );
+    let n4 = MimeNode.create('text/html', {
+        filename: 'jõgeva kalasadama tee ristmiku kalde künka.txt',
+        encoding: 'quoted-printable'
+    });
+
+    n4.content =
+        'Οὐχὶ ταὐτὰ παρίσταταί μοι γιγνώσκειν, ὦ ἄνδρες ᾿Αθηναῖοι, ὅταν τ᾿ εἰς τὰ πράγματα ἀποβλέψω καὶ ὅταν πρὸς τοὺς λόγους οὓς ἀκούω· τοὺς μὲν γὰρ λόγους περὶ τοῦ τιμωρήσασθαι Φίλιππον ὁρῶ γιγνομένους, τὰ δὲ πράγματ᾿';
 
     n1.appendChild(n2);
     n2.appendChild(n3);
