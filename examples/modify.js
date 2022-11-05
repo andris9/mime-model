b9f1a5afc3bafb5ee43ca55ee52bc3c7a1478d99
Andris Reinman <andris@reinman.eu>
2022-11-05 21:45:40 +0200

    Added subject and date manipulation methods

--- examples/modify.js
+++ examples/modify.js
@@ -28,17 +28,16 @@ function walkNodes(node) {
             case 'text/plain':
             case 'text/html':
                 {
-                    // node.content returns a Buffer, contentText returns unicode string
+                    // node.content returns a Buffer in the original charset, node.contentText returns a unicode string
                     let content = node.contentText;
 
                     // The text content might use non-ascii letters, so convert output encoding to quoted-printable
                     node.encoding = 'quoted-printable';
 
-                    // Convert MIME output to use Windows-1257
+                    // Enforce MIME output to use Windows-1257 for this text node
                     node.charset = 'win-1257';
 
-                    // String value is converted to correct charset
-                    // Buffer value is stored as is
+                    // String value is converted to correct charset, Buffer value is used as is
                     node.content = `${content}\n${node.contentType === 'text/html' ? `<div>${text}</div>` : text}`;
                 }
                 break;
@@ -47,5 +46,14 @@ function walkNodes(node) {
 }
 
 walkNodes(rootNode);
+
+// add the same text to the end of subject line as well
+// (encoding is handled automatically)
+rootNode.subject += ` ${text}`;
+
+if (rootNode.date) {
+    // Increment date header value by 1 day
+    rootNode.date = rootNode.date.getTime() + 1 * 24 * 3600 * 1000;
+}
 
 process.stdout.write(rootNode.serialize());
